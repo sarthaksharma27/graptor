b3ccafd3745f65f4999ada1582fafe3f96d55521
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-09-08 17:55:15 +0530

    setting up traversing on cst

--- src/generateAst.ts
+++ src/generateAst.ts
@@ -1,16 +1,21 @@
 import fg from "fast-glob";
-const Parser = require('tree-sitter');
+import Parser, { SyntaxNode } from 'tree-sitter';
 const JavaScript = require('tree-sitter-javascript');
+import fs from 'fs'
 
 const parser = new Parser();
 parser.setLanguage(JavaScript);
 
-const sourceCode = 'let x = 1; console.log(x);';
-const tree = parser.parse(sourceCode);
+const SEMANTIC_NODE_TYPES = new Set([
+  'import_statement',
+  'export_statement',
+]);
 
-console.log(tree.rootNode.toString());
-
-
+function cstToSementic(node: SyntaxNode) {
+  console.log(node);
+  
+  
+}
 
 export async function generateASTs(baseDir: string) {
   const files = await fg("**/*.{js,ts}", {
@@ -26,51 +31,8 @@ export async function generateASTs(baseDir: string) {
 
   for (const file of files) {
     console.log("Found file:", file);
+    const readedfile = fs.readFileSync(file, "utf8")
+    const tree = parser.parse(readedfile)
+    const ast = cstToSementic(tree.rootNode)
   }
 }
-
-
-
-
-// import fs from 'fs';
-// import path from 'path';
-// import { parse } from '@babel/parser';
-// import fg from 'fast-glob';
-// import defaultIgnorePatterns from './defaultIgnore';
-// import { extractSemantics } from './extractSemantics';
-
-// export function generateASTs(baseDir: string): void {
-//   const files = fg.sync(['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'], {
-//     cwd: baseDir,
-//     ignore: defaultIgnorePatterns,
-//     absolute: true,
-//   });
-
-//   const result: Record<string, any[]> = {};
-
-//   for (const file of files) {
-//     try {
-//       const code = fs.readFileSync(file, 'utf-8');
-//       const ast = parse(code, {
-//         sourceType: 'unambiguous',
-//         plugins: ['typescript', 'jsx'],
-//       });
-
-//       const semantic = extractSemantics(ast);
-//       const relativePath = path.relative(baseDir, file);
-//       result[relativePath] = semantic;
-
-//       console.log(`Parsed: ${relativePath}`);
-//     } catch (error) {
-//       console.warn(`Failed to parse ${file}: ${(error as Error).message}`);
-//     }
-//   }
-
-//   const outputDir = '.graptor';
-//   if (!fs.existsSync(outputDir)) {
-//     fs.mkdirSync(outputDir);
-//   }
-
-//   fs.writeFileSync(path.join(outputDir, 'graptor.ast.json'), JSON.stringify(result, null, 2));
-//   console.log('\n🎉 Semantic ASTs written to .graptor/graptor.ast.json');
-// }
