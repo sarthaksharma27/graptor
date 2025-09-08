a8f2c6bb3220643faefc78d24d1c9d5e6507d9ac
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-09-08 16:42:13 +0530

    feat: seting up tree-sitter

--- src/generateAst.ts
+++ src/generateAst.ts
@@ -1,4 +1,16 @@
 import fg from "fast-glob";
+const Parser = require('tree-sitter');
+const JavaScript = require('tree-sitter-javascript');
+
+const parser = new Parser();
+parser.setLanguage(JavaScript);
+
+const sourceCode = 'let x = 1; console.log(x);';
+const tree = parser.parse(sourceCode);
+
+console.log(tree.rootNode.toString());
+
+
 
 export async function generateASTs(baseDir: string) {
   const files = await fg("**/*.{js,ts}", {
