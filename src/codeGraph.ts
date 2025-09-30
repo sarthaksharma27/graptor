7cd5308e42abd7ef4f63d301062e1e7df6455bff
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-09-30 23:47:44 +0530

    Adding logic of codegraph

--- src/codeGraph.ts
+++ src/codeGraph.ts
@@ -14,7 +14,12 @@ interface edge {
     type: EdgeType 
 }
 
-export async function generateCodegraph(astMap: Record<string, SemanticNode[]>) {
-    console.log(astMap)
+export async function generateCodegraph(astMaps: Record<string, SemanticNode[]>) {
+    // console.log(astMaps)
+    for (const filePath in astMaps) {
+        const nodes = astMaps[filePath]; // nodes is SemanticNode[]
+        console.log(filePath, nodes);
+    }
+    
 }
 
