75794f229d41322f904461071fec22f608676a19
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-09-30 22:46:23 +0530

    feat: making interface of code graph

--- src/codeGraph.ts
+++ src/codeGraph.ts
@@ -1,5 +1,19 @@
 import { SemanticNode } from "./generateAst";
 
+interface node {
+    id: string;
+    type: "File" | "exported_function" | "external_module";
+    
+}
+
+type EdgeType = "exports" | "depends_on" | "requires";
+
+interface edge {
+    from: string;
+    to: string;
+    edgeType: EdgeType 
+}
+
 export async function generateCodegraph(astMap: Record<string, SemanticNode[]>) {
     console.log(astMap)
 }
