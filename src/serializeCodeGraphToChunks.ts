5f4870281e2effa2db405a54f1b242ee9a054c00
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-10-05 02:15:52 +0530

    Created a new file serializeCodeGraphToChunks.ts inside src

---
+++ src/serializeCodeGraphToChunks.ts
@@ -0,0 +1,11 @@
+import { Edge, Node } from "./codeGraph"
+
+
+interface codeGraph {
+    graphNodes: Node[]
+    graphEdges: Edge[]
+}
+
+export function serializeCodeGraphToChunks(codeGraph: codeGraph) {
+    
+}
