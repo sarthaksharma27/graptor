e8d26fe906b537f4f991dee570e0653f68d228fe
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-10-09 23:02:20 +0530

    feat: Generating vector embedding of the codebase

--- src/generateVectorEmbeddings.ts
+++ src/generateVectorEmbeddings.ts
@@ -10,17 +10,16 @@ import { pipeline } from '@xenova/transformers';
 
 export async function generateVectorEmbeddings(chunks: TextChunk[]) {
     console.log('Chunks:', chunks.length);
-    // console.log( "provider:", config?.provider, "model:", config?.model, "apiKey:", config?.apiKey);
-    
-
-        const embedder = await pipeline('feature-extraction', 'Xenova/bge-small-en-v1.5');
-
-        const text = 'Machine learning models extract features automatically.';
+    const embedder = await pipeline('feature-extraction', 'Xenova/bge-small-en-v1.5');
+    chunks.forEach(async chunk => {
+        const text = chunk.text;
         const result = await embedder(text, { pooling: 'mean', normalize: true });
 
         const embedding = result.data; 
         console.log('Embedding length:', embedding.length);
         console.log('First 5 values:', Array.from(embedding.slice(0, 5)));
+    });
+
 }
 
 
